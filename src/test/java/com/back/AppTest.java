f3a458f347576c0473796c686051df9e871608fe
junseokPP <pjs981130@gmail.com>
2025-09-24 17:04:51 +0900

    9강-red

--- src/test/java/com/back/AppTest.java
+++ src/test/java/com/back/AppTest.java
@@ -13,4 +13,18 @@ public class AppTest {
         String out = AppTestRunner.run("종료");
         assertThat(out).contains("== 명언 앱 ==");
     }
+
+    @Test
+    @DisplayName("등록")
+    void t2(){
+        String out = AppTestRunner.run("""
+                등록
+                너 자신을 알라
+                소크라테스
+                종료
+                """);
+        assertThat(out).contains("명령) ");
+        assertThat(out).contains("명언 : ");
+        assertThat(out).contains("작가 :  ");
+    }
 }
