ef5d0d96eba465b19eaa4fc289504f38e3188d49
junseokPP <pjs981130@gmail.com>
2025-10-24 11:26:57 +0900

    36강 - green

--- src/test/java/com/back/standard/util/UtilFileTest.java
+++ src/test/java/com/back/standard/util/UtilFileTest.java
@@ -9,32 +9,54 @@ public class UtilFileTest {
 
     @Test
     @DisplayName("파일 생성")
-    void t1(){
+    void t1() {
 
-        //파일 경로 지정
+        // 무언가를 세팅하고
         String filePath = "test.txt";
 
-        //수행
+        // 수행하면
         Util.file.touch(filePath);
 
-        //존재여부
+        // 결과가 나온다. => 실제 파일이 존재하는가?
         boolean rst = Util.file.exists(filePath);
         assertThat(rst).isTrue();
 
+        // 테스트가 끝나면 파일 삭제
         Util.file.delete(filePath);
+
     }
 
     @Test
-    @DisplayName("파일삭제")
-    void t2(){
+    @DisplayName("파일 삭제")
+    void t2() {
 
+        // given
         String filePath = "test.txt";
-        Util.file.touch(filePath);
+        Util.file.touch(filePath); // 파일 생성
 
+        // when
         Util.file.delete(filePath);
 
+        // then
         boolean rst = Util.file.exists(filePath);
         assertThat(rst).isFalse();
 
+    }
+
+    @Test
+    @DisplayName("파일 읽기/쓰기")
+    void t3() {
+
+        // given
+        String filePath = "test.txt";
+        Util.file.set(filePath, "hello world"); // 파일 쓰기
+
+        // when
+        String content = Util.file.get(filePath, "");
+
+        // then
+        assertThat(content).isEqualTo("hello world");
+
+        Util.file.delete(filePath);
     }
 }
